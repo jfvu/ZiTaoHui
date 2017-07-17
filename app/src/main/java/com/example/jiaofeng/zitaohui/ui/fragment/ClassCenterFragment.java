a7679033913f097d626a40bb3bfd3361ee276103
jf <190310578@qq.com>
2017-07-17 17:21:04 +0800

    class页面重新更改控件，仍未解决滑动问题

---
+++ app/src/main/java/com/example/jiaofeng/zitaohui/ui/fragment/ClassCenterFragment.java
@@ -0,0 +1,125 @@
+package com.example.jiaofeng.zitaohui.ui.fragment;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.jiaofeng.zitaohui.R;
+import com.zhy.autolayout.utils.AutoUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.Unbinder;
+
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+
+public class ClassCenterFragment extends BaseFragment {
+    @BindView(R.id.rv_classcenter)
+    RecyclerView mRvClasscenter;
+    Unbinder unbinder;
+    private int mInt;
+    private List<Entityclass> entityclasses = new ArrayList<>();
+
+    public ClassCenterFragment(int anInt) {
+        mInt = anInt;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_classcenter;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+
+        mRvClasscenter.setLayoutManager(new GridLayoutManager(getActivity(),4));
+        mRvClasscenter.setAdapter(new QuickAdapter());
+    }
+    public class QuickAdapter extends RecyclerView.Adapter<QuickAdapter.ContactsViewHolder>{
+
+
+        @Override
+        public ContactsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.item_content_rl, null);
+            AutoUtils.autoSize(view);
+            return new ContactsViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ContactsViewHolder holder, int position) {
+            List<Entityclass> list = getEetityclasses(mInt);
+
+           holder.tvIndex.setText(list.get(position).getName());
+            holder.ivAvatar.setImageResource(list.get(position).getImg() );
+            holder.mLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mInt;
+        }
+        class ContactsViewHolder extends RecyclerView.ViewHolder {
+            private LinearLayout mLayout;
+            public TextView tvIndex;
+            public ImageView ivAvatar;
+
+
+            public ContactsViewHolder(View itemView) {
+                super(itemView);
+                mLayout = (LinearLayout) itemView.findViewById(R.id.ll_item);
+                tvIndex = (TextView) itemView.findViewById(R.id.tv_name_item);
+                ivAvatar = (ImageView) itemView.findViewById(R.id.img_item);
+
+            }
+        }
+    }
+
+    public class Entityclass {
+        private int img;
+        private String name;
+
+        public Entityclass(int img, String name) {
+            this.img = img;
+            this.name = name;
+        }
+
+        public int getImg() {
+            return img;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+    }
+    private List<Entityclass> getEetityclasses(int i) {
+        for (int j = 0; j < i; j++) {
+            entityclasses.add(new Entityclass(R.drawable.rl_picture, "分类名称"));
+        }
+
+        return entityclasses;
+    }
+}
--- app/src/main/java/com/example/jiaofeng/zitaohui/ui/fragment/ClassFragment.java
+++ app/src/main/java/com/example/jiaofeng/zitaohui/ui/fragment/ClassFragment.java
@@ -1,28 +1,29 @@
 package com.example.jiaofeng.zitaohui.ui.fragment;
 
 import android.graphics.Color;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.jiaofeng.zitaohui.R;
-import com.zhy.autolayout.utils.AutoUtils;
+import com.example.jiaofeng.zitaohui.adapter.MyFregmentAdapter3;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import me.kaelaela.verticalviewpager.VerticalViewPager;
+import me.kaelaela.verticalviewpager.transforms.DefaultTransformer;
+import q.rorbin.verticaltablayout.VerticalTabLayout;
+import q.rorbin.verticaltablayout.adapter.TabAdapter;
+import q.rorbin.verticaltablayout.widget.ITabView;
+import q.rorbin.verticaltablayout.widget.TabView;
 
 /**
  * Created by jiaofeng on 2017/7/16.
@@ -31,18 +32,20 @@ import butterknife.Unbinder;
 public class ClassFragment extends BaseFragment {
     @BindView(R.id.et_input_fragment_class)
     EditText etInputFragmentClass;
-    /*@BindView(R.id.lv_class_fragment_class)
-    ListView lvClassFragmentClass;*/
     @BindView(R.id.tv_classname_fragment_class)
     TextView tvClassnameFragmentClass;
-    @BindView(R.id.rv_content_fragment_class)
-    RecyclerView rvContentFragmentClass;
     Unbinder unbinder;
+    @BindView(R.id.vtl_class_fragment_class)
+    VerticalTabLayout mVtlClassFragmentClass;
+    @BindView(R.id.yvp_fragment_class)
+    VerticalViewPager mYvpFragmentClass;
+    Unbinder unbinder1;
+
+
     private List<ClassName> classNames;
-    private MyClassNameAdapter myClassNameAdapter;
     private ArrayList<Integer> number = new ArrayList<>();
-    private List<Entityclass> entityclasses;
-    private MyRlAdapter myRlAdapter;
+    private MyFregmentAdapter3 myFregmentAdapter3;
+
     private int flag = 0;
 
 
@@ -53,36 +56,7 @@ public class ClassFragment extends BaseFragment {
 
     @Override
     protected void initData() {
-        myRlAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                Toast.makeText(getActivity(), ""+position, Toast.LENGTH_SHORT).show();
-            }
-        });
-        /*myRlAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
-            @Override
-            public void onLoadMoreRequested() {
-                if (flag != classNames.size() ){
-                    flag = flag +1;
-                    entityclasses.clear();
-                    getEetityclasses(number.get(flag));
-                    myRlAdapter.notifyDataSetChanged();
-                }
 
-            }
-        });
-        myRlAdapter.setUpFetchListener(new BaseQuickAdapter.UpFetchListener() {
-            @Override
-            public void onUpFetch() {
-                if (flag != 0){
-                    flag = flag -1;
-                    entityclasses.clear();
-                    getEetityclasses(number.get(flag));
-                    myRlAdapter.notifyDataSetChanged();
-
-                }
-            }
-        });*/
     }
 
     @Override
@@ -100,53 +74,98 @@ public class ClassFragment extends BaseFragment {
         classNames.add(name3);
         classNames.add(name4);
         classNames.add(name5);
+        tvClassnameFragmentClass.setText(classNames.get(0).getName());
 
-        entityclasses = new ArrayList<>();
-        entityclasses.add(new Entityclass(R.drawable.rl_picture,"分类名称"));
         number.add(24);
         number.add(8);
         number.add(33);
         number.add(55);
         number.add(66);
         number.add(77);
-
-
-        /*myClassNameAdapter = new MyClassNameAdapter();
-        lvClassFragmentClass.setAdapter(new MyClassNameAdapter());
-        tvClassnameFragmentClass.setText(classNames.get(0).getName());
-        lvClassFragmentClass.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        myFregmentAdapter3 = new MyFregmentAdapter3(getChildFragmentManager(),getActivity());
+        mYvpFragmentClass.setAdapter(myFregmentAdapter3);
+        mYvpFragmentClass.setPageTransformer(true, new DefaultTransformer());
+        mVtlClassFragmentClass.setupWithViewPager(mYvpFragmentClass);
+        mVtlClassFragmentClass.addOnTabSelectedListener(new VerticalTabLayout.OnTabSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                for (int i = 0; i < classNames.size(); i++) {
-                    parent.getChildAt(i).setBackgroundColor(Color.parseColor("#F9F9F9"));
-                }
-                view.setBackgroundColor(Color.parseColor("#ffffff"));
+            public void onTabSelected(TabView tab, int position) {
                 tvClassnameFragmentClass.setText(classNames.get(position).getName());
-                entityclasses.clear();
-                getEetityclasses(number.get(position));
-                myRlAdapter.notifyDataSetChanged();
-                flag = position;
+            }
+
+            @Override
+            public void onTabReselected(TabView tab, int position) {
 
             }
-        });*/
-        myRlAdapter = new MyRlAdapter(R.layout.item_content_rl,getEetityclasses(number.get(0)));
-        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        //linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        rvContentFragmentClass.setLayoutManager(new GridLayoutManager(getActivity(),4));
-        rvContentFragmentClass.setAdapter(myRlAdapter);
+        });
+        mVtlClassFragmentClass.setTabAdapter(new TabAdapter() {
+            @Override
+            public int getCount() {
+                return classNames.size();
+            }
+
+            @Override
+            public ITabView.TabBadge getBadge(int position) {
+                return null;
+            }
+
+            @Override
+            public ITabView.TabIcon getIcon(int position) {
+                return null;
+            }
+
+            @Override
+            public ITabView.TabTitle getTitle(int position) {
+                return new ITabView.TabTitle.Builder().setContent(classNames.get(position).getName()).setTextSize(14).
+                        setTextColor(Color.parseColor("#242424"), Color.parseColor("#242424")).build();
+            }
+
+            @Override
+            public int getBackground(int position) {
+                return 0;
+            }
+        });
+
+        //mYvpFragmentClass.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mVtlClassFragmentClass));
+        mYvpFragmentClass.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        //mVtlClassFragmentClass.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mYvpFragmentClass));
+
 
     }
 
-    private List<Entityclass> getEetityclasses(int i) {
-        for (int j = 0; j < i; j++) {
-            entityclasses.add(new Entityclass(R.drawable.rl_picture,"分类名称"));
-        }
 
-        return entityclasses;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder1 = ButterKnife.bind(this, rootView);
+        return rootView;
     }
 
-    class ClassName{
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder1.unbind();
+    }
+
+
+    class ClassName {
         private String name;
 
         public ClassName(String name) {
@@ -157,72 +176,8 @@ public class ClassFragment extends BaseFragment {
             return name;
         }
     }
-    class Entityclass{
-        private int img;
-        private String name;
-
-        public Entityclass(int img, String name) {
-            this.img = img;
-            this.name = name;
-        }
-
-        public int getImg() {
-            return img;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
-
-    private class MyClassNameAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return classNames.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return classNames.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LinearLayout linearLayout;
-            TextView textView;
-            View view = null;
-            if (convertView !=null){
-                view = convertView;
-            }else {
-                view = View.inflate(getContext(), R.layout.item_classnamelv,null);
-            }
-            AutoUtils.autoSize(view);
-            linearLayout = (LinearLayout) view.findViewById(R.id.ll_classname_lv);
-
-            textView = (TextView) view.findViewById(R.id.tv_classname_lv);
-            textView.setText(classNames.get(position).getName());
-            view.setBackgroundColor(Color.parseColor("#F9F9F9"));
-            if (position == 0){
-                view.setBackgroundColor(Color.parseColor("#ffffff"));
-            }
-            return view;
-        }
-    }
-    public class MyRlAdapter extends BaseQuickAdapter<Entityclass,BaseViewHolder>{
-
-        public MyRlAdapter(@LayoutRes int layoutResId, @Nullable List<Entityclass> data) {
-            super(R.layout.item_content_rl, entityclasses);
-        }
-
-        @Override
-        protected void convert(BaseViewHolder helper, Entityclass item) {
 
 
-        }
-    }
+
+
 }
