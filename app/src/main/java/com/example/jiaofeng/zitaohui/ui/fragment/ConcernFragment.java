10436e9a13181fa53ffbe36dc4bd9e32393b7e87
jf <190310578@qq.com>
2017-07-19 17:54:10 +0800

    ConcernFragment第一次提交

--- app/src/main/java/com/example/jiaofeng/zitaohui/ui/fragment/ConcernFragment.java
+++ app/src/main/java/com/example/jiaofeng/zitaohui/ui/fragment/ConcernFragment.java
@@ -1,12 +1,31 @@
 package com.example.jiaofeng.zitaohui.ui.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.jiaofeng.zitaohui.R;
+import com.zhy.autolayout.utils.AutoUtils;
+
+import butterknife.BindView;
+import butterknife.Unbinder;
 
 /**
  * Created by jiaofeng on 2017/7/16.
  */
 
 public class ConcernFragment extends BaseFragment {
+    @BindView(R.id.rv_fragment_concer)
+    RecyclerView mRvFragmentConcer;
+    Unbinder unbinder;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_concern;
@@ -19,6 +38,46 @@ public class ConcernFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        mRvFragmentConcer.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRvFragmentConcer.setAdapter(new ConcernFragment.MyAdapter());
+    }
 
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.item_rv_fragment_concer, parent, false);
+            AutoUtils.autoSize(view);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            private ImageView mView;
+            private TextView mView1;
+            private TextView mView2;
+            private Button mButton;
+            private RelativeLayout mLayout;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mView = (ImageView) itemView.findViewById(R.id.img_fragment_concern);
+            }
+        }
     }
 }
