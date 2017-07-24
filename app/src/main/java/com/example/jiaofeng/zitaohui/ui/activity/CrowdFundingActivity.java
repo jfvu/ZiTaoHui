947d349bda7f322dae301cdd3faff1662d22f5a1
jiaofeng <190310578@qq.com>
2017-07-24 09:03:19 +0800

    众筹

---
+++ app/src/main/java/com/example/jiaofeng/zitaohui/ui/activity/CrowdFundingActivity.java
@@ -0,0 +1,36 @@
+package com.example.jiaofeng.zitaohui.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.jiaofeng.zitaohui.R;
+
+public class CrowdFundingActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_crowd_funding;
+    }
+
+    @Override
+    protected LinearLayout getStatusParams() {
+        return null;
+    }
+
+    @Override
+    protected int color() {
+        return 0;
+    }
+}
